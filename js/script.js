0a46c1c1b22e05c6b03d17efe696e4ddda7ac794
Mallikharjuna Chevuru <mallikharjuna.chevuru@ltimindtree.com>
2026-04-22 21:55:18 -0500

    Add TAJ Consultancy website

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const navToggle = document.querySelector("#navToggle");
+const siteNav = document.querySelector("#siteNav");
+
+if (navToggle && siteNav) {
+  navToggle.addEventListener("click", () => {
+    const isOpen = siteNav.classList.toggle("is-open");
+    navToggle.setAttribute("aria-expanded", String(isOpen));
+  });
+
+  siteNav.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      siteNav.classList.remove("is-open");
+      navToggle.setAttribute("aria-expanded", "false");
+    });
+  });
+}
